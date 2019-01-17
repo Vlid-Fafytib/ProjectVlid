de91c0069e3d3bd300f41858759b160107fc7cbe
Vlid-Fafytib <fafytibvlid@gmail.com>
2019-01-17 23:21:37 +0200

    start a output

--- code/script.js
+++
@@ -1,7 +0,0 @@
-const preObject = document.getElementById("object");
-
-const dbRefObject = firebase.database().ref().child("object");
-
-//Sync
-
-dbRefObject.on("value", projectvlid => console.log(projectvlid.val()));
--- public/js/script.js
+++ public/js/script.js
@@ -3,150 +3,150 @@ const preObject = document.getElementById('all');
 const db = firebase.database();
 let listRef = db.ref().child('all');
 var array = [];
-//Sync
-// db.on('value',  function (snap) {
-
-// snap.forEach(function(childSnapshot) {
-//     var childData = childSnapshot.val();
-//     console.log(childData);
-//     console.log("d");
-// });
-
-// });
+var tag = [];
+var newItem = "";
+var bench = document.getElementById("myform");
 
 const page1 = {
-    post: document.getElementById("probleme"),
+    post1: document.getElementById("probleme"),
+    post2: document.getElementById("location"),
+    post3: document.getElementById("ache"),
     ref: db.ref('all/articles'),
+    tags: [],
     list: [],
     show: function(item) {
     },
 createOptions: function(item){
-    this.post.innerHTML = "";
+    this.post1.innerHTML = "";
+    this.post2.innerHTML = "";
+    this.post3.innerHTML = "";
     for (let i in item){
-        this.post.innerHTML+= `<option value="${i}">${item[i].title}</option>`;
+        // this.tags.push(item.val().tags.split(","));
+        this.tags[i] = item[i].tags.split(',');
+        this.post1.innerHTML+= `<option value="${this.tags[i][0]}">${this.tags[i][0]}</option>`;
+        this.post2.innerHTML+= `<option value="${this.tags[i][1]}">${this.tags[i][1]}</option>`;
+        this.post3.innerHTML+= `<option value="${this.tags[i][2]}">${this.tags[i][2]}</option>`;
     }
 }
 };
-page1.ref.on('value', function(snap) {  //функция создания списка с комментами
-	this.list = snap.val();             //массив из строки 6 заполняется даннми из базы
-	page1.createOptions(this.list);     //вызов функции для вывода вариантов списка из строки 12
-});
-// var id = "";
-// id = "id" + 1;
-
-// listRef = db.ref('all/articles/'+id);         //изменение пути листа с компментами 
-// listRef.on('value', function (snap) {     //если не выбрано ничего, то загруждается дефолтное значение пути
-//     document.querySelector('.com_field').innerHTML = "";    
-//     var data = snap.val();
-//     addItem(data);                          //опять же вызов функции добавления
-
-// });
-// for (let i = 1; i < 2; i++)voidd(i);
-// function voidd(k) {
-//     var id = "id"+2;
-//     listRef = db.ref('all/articles/'+id);         //изменение пути листа с компментами 
-//     listRef.on('value', function (snap) {     //если не выбрано ничего, то загруждается дефолтное значение пути
-//         document.querySelector('.com_field').innerHTML = "";    
-//         var data = snap.val();
-//         addItem(data);                          //опять же вызов функции добавления
-
-//     });
-// }
-listRef = db.ref('all/articles/'); //это изменение пути для комментов, в зависимости от выбранного варианта
-listRef.on('child_added', function (data) {             //функция которая вызовет другую функцию с данными получеными из бд
-        addItem(data);                                      //вызов функции addItem
+// var intervalB = setInterval(function () { // вызов добавления интервалом, данный интервал нужно остановить!!!!
+function call1() {
+    newItem = "";
+    listRef = db.ref('all/articles/'); //это изменение пути для комментов, в зависимости от выбранного варианта
+    listRef.on('child_added', function (data) {             //функция которая вызовет другую функцию с данными получеными из бд
+    tag.length = 0;
+    
+    tag.push(data.val().tags.split(","));
+        addItem(data, tag);                                      //вызов функции addItem
     });
-function addItem(item) {//добавление элемента в отображение
+}
+// }, 1000);
+
+page1.ref.on('value', function(snap) {  //функция создания списка с комментами
+    this.list = snap.val();               //массив из строки 6 заполняется даннми из базы
+    page1.createOptions(this.list);     //вызов функции для вывода вариантов списка из строки 12
+});
+function addItem(item, tag) {//добавление элемента в отображение
     //cоздадим новый элемент списка с данными из аргумента (каждый item состоит из ключа и значения)
     //по сути это просто добавление комеентария в список комментариев
-      
-    var newItem = '<div class="comm_block"><h2 class="user_name">'
-    +item.val().title+'</h2><p class="commText">'
-    +item.val().text+'</div>';
-    document.querySelector('.com_field').innerHTML += newItem;  //добавление элемента списка в список 
+    // if (bench.style.diplay == "none") {
+        
+    // console.log(tag);
+    
+    if (tag[0][0] == array[6]){
+        newItem += '<div class="comm_block"><h2 class="user_name">'
+        +item.val().title+'</h2><p class="commText">'
+        +item.val().text+'</div>';
+    } else
+    if (tag[0][1] == array[7]){
+        newItem += '<div class="comm_block"><h2 class="user_name">'
+        +item.val().title+'</h2><p class="commText">'
+        +item.val().text+'</div>';
+    } else
+    if (tag[0][2] == array[8]){
+        newItem += '<div class="comm_block"><h2 class="user_name">'
+        +item.val().title+'</h2><p class="commText">'
+        +item.val().text+'</div>';
+    }
+    if (bench.style.display == "none"){
+        document.querySelector('.com_field').innerHTML = "";  //добавление элемента списка в список
+        document.querySelector('.com_field').innerHTML += newItem;
+
+    }
+
+
+        
+    
+          //добавление элемента списка в список
+    
+    // clearInterval(intervalB);
 }
-array[6] = document.getElementById("btn-submit");
+function loadfunc(){
+    array[6] = document.getElementById("probleme").value;
+    array[7] = document.getElementById("location").value;
+    array[8] = document.getElementById("ache").value;
+    call1();
+    alert(array[6]);
+}
+// }
+page1.post1.onchange = function () {
+    array[6] = document.getElementById("probleme").value;
+    array[7] = document.getElementById("location").value;
+    array[8] = document.getElementById("ache").value;
+    call1();
+}
+page1.post2.onchange = function () {
+    array[6] = document.getElementById("probleme").value;
+    array[7] = document.getElementById("location").value;
+    array[8] = document.getElementById("ache").value;
+    call1();
+}
+page1.post3.onchange = function () {
+    array[6] = document.getElementById("probleme").value;
+    array[7] = document.getElementById("location").value;
+    array[8] = document.getElementById("ache").value;
+    call1();
+}
 
-function CustomValidation() { }
+array[9] = document.getElementById("btn-submit");
 
-CustomValidation.prototype = {
-  // Установим пустой массив сообщений об ошибках
-  invalidities: [],
-
-  // Метод, проверяющий валидность
-  checkValidity: function(input) {
-
-    var validity = input.validity;
-
-    if (validity.patternMismatch) {
-      this.addInvalidity('This is the wrong pattern for this field');
-    }
-
-    if (validity.rangeOverflow) {
-      var max = getAttributeValue(input, 'max');
-      this.addInvalidity('The maximum value should be ' + max);
-    }
-
-    if (validity.rangeUnderflow) {
-      var min = getAttributeValue(input, 'min');
-      this.addInvalidity('The minimum value should be ' + min);
-    }
-
-  },
-
-  // Добавляем сообщение об ошибке в массив ошибок
-  addInvalidity: function(message) {
-    this.invalidities.push(message);
-  },
-
-  // Получаем общий текст сообщений об ошибках
-  getInvalidities: function() {
-    return this.invalidities.join('. \n');
-  }
-};
 
 // Добавляем обработчик клика на кнопку отправки формы
-array[6].addEventListener('click', function(e) {
-    array[0] = document.getElementById("form-age");
-    array[1] = document.getElementById("form-growth");
-    array[2] = document.getElementById("form-weight");
+
+if (array[9]) {
+    array[9].addEventListener('click', function() {
+    array[0] = document.getElementById("form-age").value;
+    array[1] = document.getElementById("form-growth").value;
+    array[2] = document.getElementById("form-weight").value; 
+    array[6] = document.getElementById("probleme").value;
+    array[7] = document.getElementById("location").value;
+    array[8] = document.getElementById("ache").value;
+
     if (document.getElementById("form-male").cheked){
-        array[3] = document.getElementById("form-male");
+        array[3] = document.getElementById("form-male").value;
     } else{
-        array[3] = document.getElementById("form-female");
+        array[3] = document.getElementById("form-female").value;
     }
     if (document.getElementById("form-pressure-yes").cheked){
-        array[4] = document.getElementById("form-pressure-yes");
+        array[4] = document.getElementById("form-pressure-yes").value;
     } else if (document.getElementById("form-pressure-no").cheked){
-        array[4] = document.getElementById("form-pressure-no");
+        array[4] = document.getElementById("form-pressure-no").value;
     } else {
-        array[4] = document.getElementById("form-pressure-possibly");
+        array[4] = document.getElementById("form-pressure-possibly").value;
     } 
 
     if (document.getElementById("form-temperature-yes").cheked) {
-        array[5] = document.getElementById("form-temperature-yes");
+        array[5] = document.getElementById("form-temperature-yes").value;
     } else if(document.getElementById("form-temperature-no").cheked){
-        array[5] = document.getElementById("form-temperature-no");
+        array[5] = document.getElementById("form-temperature-no").value;
     } else{
-        array[5] = document.getElementById("form-temperature-possibly");
-    }
-
+        array[5] = document.getElementById("form-temperature-possibly").value;
+    };
+    bench.style.display='none';//visibility = "collapse";
+    call1();
   // Пройдёмся по всем полям
-  for (var i = 0; i < array.length; i++) {
 
-    var input = array[i];
-
-    // Проверим валидность поля, используя  функцию checkValidity()
-    if (input.checkValidity() == false) {
-      var inputCustomValidation = new CustomValidation(); // Создадим объект CustomValidation
-      inputCustomValidation.checkValidity(input); // Выявим ошибки
-      var customValidityMessage = inputCustomValidation.getInvalidities(); // Получим все сообщения об ошибках
-      input.setCustomValidity(customValidityMessage); // Установим специальное сообщение об ошибке
-
-    } // закончился if
-  } // закончился цикл
 
 });
 
-
-
+}
