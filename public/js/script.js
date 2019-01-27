40774769a1cd143a116dcdba209acdbc0eccff0b
Vlid-Fafytib <fafytibvlid@gmail.com>
2019-01-27 18:22:21 +0200

    a few style changes

--- public/js/script.js
+++ public/js/script.js
@@ -168,22 +168,22 @@ if (array[9]) {
     array[7] = document.getElementById("location").value;
     array[8] = document.getElementById("ache").value;
 
-    if (document.getElementById("form-male").cheked){
+    if (document.getElementById("form-male").cheсked){
         array[3] = document.getElementById("form-male").value;
     } else{
         array[3] = document.getElementById("form-female").value;
     }
-    if (document.getElementById("form-pressure-yes").cheked){
+    if (document.getElementById("form-pressure-yes").cheсked){
         array[4] = document.getElementById("form-pressure-yes").value;
-    } else if (document.getElementById("form-pressure-no").cheked){
+    } else if (document.getElementById("form-pressure-no").cheсked){
         array[4] = document.getElementById("form-pressure-no").value;
     } else {
         array[4] = document.getElementById("form-pressure-possibly").value;
     } 
 
-    if (document.getElementById("form-temperature-yes").cheked) {
+    if (document.getElementById("form-temperature-yes").cheсked) {
         array[5] = document.getElementById("form-temperature-yes").value;
-    } else if(document.getElementById("form-temperature-no").cheked){
+    } else if(document.getElementById("form-temperature-no").cheсked){
         array[5] = document.getElementById("form-temperature-no").value;
     } else{
         array[5] = document.getElementById("form-temperature-possibly").value;
